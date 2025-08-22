88d80df79f5844c24bfd2f73b006639454c696d9
sneha-s-chaudhari <snehas@vempower.org>
2025-08-23 00:23:10 +0530

    Update Cypress tests and add mochawesome reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,31 @@
-const { defineConfig } = require("cypress");
+/*const { defineConfig } = require("Cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});*/
+
+const { defineConfig } = require('Cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
